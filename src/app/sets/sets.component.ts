d842cd755ced4fa3584bc05a44496333c7477508
rosenKadrev <roro_901@abv.bg>
2024-02-19 10:10:53 +0200

    some changes

--- src/app/sets/sets.component.ts
+++ src/app/sets/sets.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     standalone: true,
+    imports: [CommonModule,],
     selector: 'app-sets',
     templateUrl: './sets.component.html',
     styleUrls: ['./sets.component.css']
 })
-export class SetsComponent { }
+export class SetsComponent {
+    private router = inject(Router);
+
+    public kurs = [{ id: 'fdfdfdfdfdfdf', name: 'kur' }, { id: 'fdfdfdfdgdfdf', name: 'kur1' }, { id: 'fdfdfd3dfdfdf', name: 'kur2' }];
+
+    onKur(id: string) {
+        console.log(id);
+        this.router.navigate([`/sets/${id}`]);
+
+    }
+}
